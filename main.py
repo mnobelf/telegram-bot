e1f42de7390c7781705bcbf3d062531f0c391b3c
Nobelf <13517042@std.stei.itb.ac.id>
2022-10-06 11:33:42 +0700

    add template

--- main.py
+++ main.py
@@ -9,8 +9,21 @@ API_KEY = os.getenv('API_KEY')
 
 bot = telebot.TeleBot(API_KEY)
 
-@bot.message_handler(commands=['Greet'])
+# @bot.message_handler(commands=['Greet'])
+# def greet(message):
+#     bot.reply_to(message, "Hello!")
+
+def greet_handler(message):
+    # print(message)
+    if message.text == "Hello":
+        # print("True")
+        return True
+    else:
+        # print("False")
+        return False
+
+@bot.message_handler(func=greet_handler)
 def greet(message):
-    bot.reply_to(message, "Hello!")
+    bot.send_message(message.chat.id, "Hello!")
 
 bot.polling()
