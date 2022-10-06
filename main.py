ad32df52711655da89b441719b621d2e46dd66ca
Nobelf <13517042@std.stei.itb.ac.id>
2022-10-06 18:24:38 +0700

    add some question answer pairs

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import os
 import telebot
 from dotenv import load_dotenv
+import sqlite3
+import pandas as pd
+from difflib import get_close_matches
+
+con = sqlite3.connect("qa.sqlite3")
+
+# cur = con.cursor()
+
+# qa_pair = cur.execute("SELECT question, answer FROM qa_pair;")
+# print(qa_pair[1])
+
+# for row in qa_pair:
+#     print(row)
+
+dataframe = pd.read_sql_query("SELECT question, answer FROM qa_pair", con)
 
 load_dotenv()
 
@@ -13,17 +28,26 @@ bot = telebot.TeleBot(API_KEY)
 # def greet(message):
 #     bot.reply_to(message, "Hello!")
 
-def greet_handler(message):
-    # print(message)
-    if message.text == "Hello":
-        # print("True")
-        return True
-    else:
-        # print("False")
-        return False
+# def greet_handler(message):
+#     # print(message)
+#     if message.text == "Hello":
+#         # print("True")
+#         return True
+#     else:
+#         # print("False")
+#         return False
 
-@bot.message_handler(func=greet_handler)
-def greet(message):
-    bot.send_message(message.chat.id, "Hello!")
+# @bot.message_handler(func=greet_handler)
+# def greet(message):
+#     bot.send_message(message.chat.id, "Hello!")
+
+@bot.message_handler(func=lambda m: True)
+def reply(message):
+    q = get_close_matches(message.text,dataframe['question'])
+    # print(q[0])
+    ans = dataframe.loc[dataframe['question'] == q[0], 'answer'].iloc[0]
+    # print(ans)
+    bot.send_message(message.chat.id,ans)
+
 
 bot.polling()
